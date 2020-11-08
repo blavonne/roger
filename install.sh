94ee50531be05cb2a08c79a61f9550276dcbc4d6
blavonne <luchik_sonza@mail.ru>
2020-11-08 07:17:57 +0300

    install

--- install.sh
+++ install.sh
@@ -12,17 +12,21 @@ echo -e "${sgreen}[net-tools is installed]${egreen}"
 apt-get install ufw -y
 echo -e "${sgreen}[ufw is installed]${egreen}"
 
+echo -e "${sgreen}[Add barrett? y/n]${egreen}"
+read item
+case "$item" in
+  y|Y)
 sed -ie '/^# Allow members/i barrett ALL=(ALL:ALL) ALL' /etc/sudoers
+echo -e "${sgreen}[barrett added to sudoers]${egreen}"
+  ;;
+  n|N)
+  ;;
+esac
 
-sed -ie 's/#Port 22/Port 50000/' /etc/ssh/sshd_config
-echo -e "${sgreen}[Port changed.]${egreen}"
-sed -ie 's/#PermitRootLogin prohibit-password/PermitRootLogin no/' /etc/ssh/sshd_config
-echo -e "${sgreen}[PermitRootLogin changed.]${egreen}"
-sed -ie 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
-echo -e "${sgreen}[PasswordAuthentification changed.]${egreen}"
-sed -ie 's/#PubkeyAuthentication/PubkeyAuthentication/' /etc/ssh/sshd_config
-echo -e "${sgreen}[PubkeyAuthentication changed.]${egreen}"
-
+echo -e "${sgreen}[Change network config? y/n]${egreen}"
+read item
+case "$item" in
+  y|Y)
 touch /etc/network/interfaces.d/enp0s8
 echo -e "${sgreen}[enp0s8-config created.]${egreen}"
 echo 'auto enp0s8' >> /etc/network/interfaces
@@ -30,8 +34,29 @@ echo 'iface enp0s8 inet static' >> /etc/network/interfaces.d/enp0s8
 echo 'address 192.168.1.42' >> /etc/network/interfaces.d/enp0s8
 echo 'netmask 255.255.255.252' >> /etc/network/interfaces.d/enp0s8
 echo -e "${sgreen}[enp0s8-config set.]${egreen}"
+  ;;
+  n|N)
+  ;;
+esac
 
-echo -e "${sgreen}[Confuguration is done. Reboot? y/n]${egreen}"
+echo -e "${sgreen}[Change SSH config? y/n]${egreen}"
+read item
+case "$item" in
+  y|Y)
+sed -ie 's/#Port 22/Port 50000/' /etc/ssh/sshd_config
+echo -e "${sgreen}[Port changed.]${egreen}"
+sed -ie 's/#PermitRootLogin prohibit-password/PermitRootLogin no/' /etc/ssh/sshd_config
+echo -e "${sgreen}[PermitRootLogin changed.]${egreen}"
+sed -ie 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+echo -e "${sgreen}[PasswordAuthentication changed.]${egreen}"
+sed -ie 's/#PubkeyAuthentication/PubkeyAuthentication/' /etc/ssh/sshd_config
+echo -e "${sgreen}[PubkeyAuthentication changed.]${egreen}"
+  ;;
+  n|N)
+  ;;
+esac
+
+echo -e "${sgreen}[Reboot? y/n]${egreen}"
 read item
 case "$item" in
     y|Y) echo -e "${sgreen}[Reboot...]${egreen}"
