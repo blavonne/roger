9237d364800d93e43e87f302132760669f42ea6d
blavonne <luchik_sonza@mail.ru>
2020-11-14 17:33:14 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,47 +4,30 @@ green=\\e[1\;32m
 rose=\\e[1\;1\;35m
 end_c=\\e[0m
 #-------------------------------------------------------------------------------
-echo -en "${rose}Updating system [${end_c}"
-@sudo apt-get update && sudo apt-get upgrade -y
-echo -en "${rose}.] ${end_c}"
+echo -e "${rose}Updating system${end_c}"
+sudo apt-get update && sudo apt-get upgrade -y
 echo -e "${green}[done]${end_c}"
-echo -en "${rose}Cloning from github [${end_c}"
-@git clone git@github.com:blavonne/roger.git
-echo -en "${rose}.] ${end_c}"
+echo -e "${rose}Cloning from github${end_c}"
+git clone git@github.com:blavonne/roger.git
 echo -e "${green}[done]${end_c}"
-echo -en "${rose}Installing apache2 && php [${end_c}"
-@sudo apt-get apache2 -y
-echo -en "${rose}.${end_c}"
-@sudo apt install php libapache2-mod-php php -y
-echo -en "${rose}.] ${end_c}"
+echo -e "${rose}Installing apache2 && php${end_c}"
+sudo apt-get apache2 -y
+sudo apt install php libapache2-mod-php php -y
 echo -e "${green}[done]${end_c}"
-echo -en "${rose}Repair apache-config [${end_c}"
-@sudo rm -rf /var/www/barni21.com
-echo -en "${rose}.${end_c}"
-@sudo a2enmod ssl
-echo -en "${rose}.${end_c}"
-@sudo cp -rf ./roger/Web-part/barni21.com/ /var/www/
-echo -en "${rose}.${end_c}"
-@sudo rm -f /etc/apache2/sites-available/barni21-ssl.conf
-echo -en "${rose}.${end_c}"
-@sudo cp -f ./roger/Web-part/barni21-ssl.conf /etc/apache2/sites-available/
-echo -en "${rose}.${end_c}"
-@sudo rm -f /etc/apache2/sites-available/barni21.com.conf
-echo -en "${rose}.${end_c}"
-@sudo cp ./roger/Web-part/barni21.com.conf /etc/apache2/sites-available/
-echo -en "${rose}.${end_c}"
-@sudo a2dissite 000-default.conf
-echo -en "${rose}.${end_c}"
-@sudo a2ensite barni21.com
-echo -en "${rose}.${end_c}"
-@sudo a2ensite barni21-ssl
-echo -en "${rose}.${end_c}"
-@sudo cp -f /roger/dir.conf /etc/apache2/mods-enabled/
-echo -en "${rose}.${end_c}"
-@sudo systemctl restart apache2
-echo -en "${rose}.] ${end_c}"
+echo -e "${rose}Repair apache-config${end_c}"
+sudo rm -rf /var/www/barni21.com
+sudo a2enmod ssl
+sudo cp -rf ./roger/Web-part/barni21.com/ /var/www/
+sudo rm -f /etc/apache2/sites-available/barni21-ssl.conf
+sudo cp -f ./roger/Web-part/barni21-ssl.conf /etc/apache2/sites-available/
+sudo rm -f /etc/apache2/sites-available/barni21.com.conf
+sudo cp ./roger/Web-part/barni21.com.conf /etc/apache2/sites-available/
+sudo a2dissite 000-default.conf
+sudo a2ensite barni21.com
+sudo a2ensite barni21-ssl
+sudo cp -f /roger/dir.conf /etc/apache2/mods-eabled/
+sudo systemctl restart apache2
 echo -e "${green}[done]${end_c}"
-echo -en "${rose}Cleaning [${end_c}"
-@sudo rm -rf roger/
-echo -en "${rose}.] ${end_c}"
+echo -e "${rose}Cleaning${end_c}"
+sudo rm -rf roger/
 echo -e "${green}[done]${end_c}"
